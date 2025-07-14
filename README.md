b5c5e9b344cde3947245f1674f0e600e1339c625
Damon Fedorick <95268795+Ddemon26@users.noreply.github.com>
2025-07-14 11:49:47 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# DreamCompiler
+A Nightmare
---
+++ dream.c
@@ -0,0 +1,309 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+// Token types
+typedef enum {
+    TOKEN_INT, TOKEN_IDENTIFIER, TOKEN_NUMBER, TOKEN_PLUS, TOKEN_SEMICOLON,
+    TOKEN_EQUALS, TOKEN_CONSOLE, TOKEN_DOT, TOKEN_WRITELINE, TOKEN_LPAREN,
+    TOKEN_RPAREN, TOKEN_EOF, TOKEN_UNKNOWN
+} TokenType;
+
+// Token structure
+typedef struct {
+    TokenType type;
+    char* value;
+} Token;
+
+// Node types for AST
+typedef enum {
+    NODE_VAR_DECL, NODE_ASSIGN, NODE_WRITELINE, NODE_BINARY_OP
+} NodeType;
+
+// AST node structure
+typedef struct Node {
+    NodeType type;
+    char* value; // For identifiers, numbers, or operators
+    struct Node* left;
+    struct Node* right;
+} Node;
+
+// Lexer state
+typedef struct {
+    char* source;
+    int pos;
+    int line;
+} Lexer;
+
+// Compiler context
+typedef struct {
+    FILE* output;
+    int var_count;
+} Compiler;
+
+// Lexer functions
+Token next_token(Lexer* lexer) {
+    while (isspace(lexer->source[lexer->pos])) lexer->pos++;
+
+    Token token = {TOKEN_UNKNOWN, NULL};
+    if (lexer->source[lexer->pos] == '\0') {
+        token.type = TOKEN_EOF;
+        return token;
+    }
+
+    if (isalpha(lexer->source[lexer->pos])) {
+        int start = lexer->pos;
+        while (isalnum(lexer->source[lexer->pos])) lexer->pos++;
+        int len = lexer->pos - start;
+        token.value = strndup(lexer->source + start, len);
+        if (strcmp(token.value, "int") == 0) token.type = TOKEN_INT;
+        else if (strcmp(token.value, "Console") == 0) token.type = TOKEN_CONSOLE;
+        else if (strcmp(token.value, "WriteLine") == 0) token.type = TOKEN_WRITELINE;
+        else token.type = TOKEN_IDENTIFIER;
+        return token;
+    }
+
+    if (isdigit(lexer->source[lexer->pos])) {
+        int start = lexer->pos;
+        while (isdigit(lexer->source[lexer->pos])) lexer->pos++;
+        int len = lexer->pos - start;
+        token.value = strndup(lexer->source + start, len);
+        token.type = TOKEN_NUMBER;
+        return token;
+    }
+
+    switch (lexer->source[lexer->pos]) {
+        case '+': token.type = TOKEN_PLUS; token.value = strdup("+"); lexer->pos++; break;
+        case ';': token.type = TOKEN_SEMICOLON; token.value = strdup(";"); lexer->pos++; break;
+        case '=': token.type = TOKEN_EQUALS; token.value = strdup("="); lexer->pos++; break;
+        case '.': token.type = TOKEN_DOT; token.value = strdup("."); lexer->pos++; break;
+        case '(': token.type = TOKEN_LPAREN; token.value = strdup("("); lexer->pos++; break;
+        case ')': token.type = TOKEN_RPAREN; token.value = strdup(")"); lexer->pos++; break;
+        default: token.type = TOKEN_UNKNOWN; token.value = strndup(lexer->source + lexer->pos++, 1); break;
+    }
+    return token;
+}
+
+// Parser functions
+Node* create_node(NodeType type, char* value, Node* left, Node* right) {
+    Node* node = malloc(sizeof(Node));
+    node->type = type;
+    node->value = value ? strdup(value) : NULL;
+    node->left = left;
+    node->right = right;
+    return node;
+}
+
+Node* parse_statement(Lexer* lexer, Token* token) {
+    if (token->type == TOKEN_INT) {
+        free(token->value);
+        *token = next_token(lexer);
+        if (token->type != TOKEN_IDENTIFIER) {
+            fprintf(stderr, "Expected identifier after int\n");
+            exit(1);
+        }
+        char* var_name = token->value;
+        *token = next_token(lexer);
+        if (token->type != TOKEN_SEMICOLON) {
+            fprintf(stderr, "Expected semicolon\n");
+            exit(1);
+        }
+        return create_node(NODE_VAR_DECL, var_name, NULL, NULL);
+    } else if (token->type == TOKEN_IDENTIFIER) {
+        char* var_name = token->value;
+        *token = next_token(lexer);
+        if (token->type == TOKEN_EQUALS) {
+            free(token->value);
+            *token = next_token(lexer);
+            if (token->type == TOKEN_IDENTIFIER || token->type == TOKEN_NUMBER) {
+                Node* left = create_node(token->type == TOKEN_IDENTIFIER ? NODE_VAR_DECL : NODE_BINARY_OP, token->value, NULL, NULL);
+                *token = next_token(lexer);
+                if (token->type == TOKEN_PLUS) {
+                    char* op = token->value;
+                    *token = next_token(lexer);
+                    if (token->type != TOKEN_IDENTIFIER && token->type != TOKEN_NUMBER) {
+                        fprintf(stderr, "Expected identifier or number after +\n");
+                        exit(1);
+                    }
+                    Node* right = create_node(token->type == TOKEN_IDENTIFIER ? NODE_VAR_DECL : NODE_BINARY_OP, token->value, NULL, NULL);
+                    *token = next_token(lexer);
+                    if (token->type != TOKEN_SEMICOLON) {
+                        fprintf(stderr, "Expected semicolon\n");
+                        exit(1);
+                    }
+                    return create_node(NODE_BINARY_OP, op, left, right);
+                } else if (token->type == TOKEN_SEMICOLON) {
+                    return create_node(NODE_ASSIGN, var_name, left, NULL);
+                }
+            }
+            fprintf(stderr, "Invalid assignment\n");
+            exit(1);
+        }
+    } else if (token->type == TOKEN_CONSOLE) {
+        free(token->value);
+        *token = next_token(lexer);
+        if (token->type != TOKEN_DOT) {
+            fprintf(stderr, "Expected dot after Console\n");
+            exit(1);
+        }
+        free(token->value);
+        *token = next_token(lexer);
+        if (token->type != TOKEN_WRITELINE) {
+            fprintf(stderr, "Expected WriteLine\n");
+            exit(1);
+        }
+        free(token->value);
+        *token = next_token(lexer);
+        if (token->type != TOKEN_LPAREN) {
+            fprintf(stderr, "Expected (\n");
+            exit(1);
+        }
+        free(token->value);
+        *token = next_token(lexer);
+        if (token->type != TOKEN_IDENTIFIER && token->type != TOKEN_NUMBER) {
+            fprintf(stderr, "Expected identifier or number\n");
+            exit(1);
+        }
+        Node* arg = create_node(token->type == TOKEN_IDENTIFIER ? NODE_VAR_DECL : NODE_BINARY_OP, token->value, NULL, NULL);
+        *token = next_token(lexer);
+        if (token->type != TOKEN_RPAREN) {
+            fprintf(stderr, "Expected )\n");
+            exit(1);
+        }
+        free(token->value);
+        *token = next_token(lexer);
+        if (token->type != TOKEN_SEMICOLON) {
+            fprintf(stderr, "Expected semicolon\n");
+            exit(1);
+        }
+        return create_node(NODE_WRITELINE, NULL, arg, NULL);
+    }
+    fprintf(stderr, "Invalid statement\n");
+    exit(1);
+}
+
+// Code generation
+void generate_assembly(Compiler* compiler, Node* node) {
+    static int var_index = 0;
+    static char* var_map[100];
+
+    if (node->type == NODE_VAR_DECL) {
+        var_map[var_index++] = node->value;
+        fprintf(compiler->output, "    mov rax, 0\n");
+        fprintf(compiler->output, "    mov [rsp-%d], rax\n", var_index * 8);
+        compiler->var_count++;
+    } else if (node->type == NODE_ASSIGN) {
+        if (node->left->type == NODE_BINARY_OP) {
+            fprintf(compiler->output, "    mov rax, %s\n", node->left->value);
+            for (int i = 0; i < compiler->var_count; i++) {
+                if (strcmp(var_map[i], node->value) == 0) {
+                    fprintf(compiler->output, "    mov [rsp-%d], rax\n", (i + 1) * 8);
+                    break;
+                }
+            }
+        }
+    } else if (node->type == NODE_BINARY_OP) {
+        if (node->left->type == NODE_BINARY_OP) {
+            fprintf(compiler->output, "    mov rax, %s\n", node->left->value);
+        } else {
+            for (int i = 0; i < compiler->var_count; i++) {
+                if (strcmp(var_map[i], node->left->value) == 0) {
+                    fprintf(compiler->output, "    mov rax, [rsp-%d]\n", (i + 1) * 8);
+                    break;
+                }
+            }
+        }
+        if (node->right->type == NODE_BINARY_OP) {
+            fprintf(compiler->output, "    mov rbx, %s\n", node->right->value);
+        } else {
+            for (int i = 0; i < compiler->var_count; i++) {
+                if (strcmp(var_map[i], node->right->value) == 0) {
+                    fprintf(compiler->output, "    mov rbx, [rsp-%d]\n", (i + 1) * 8);
+                    break;
+                }
+            }
+        }
+        if (strcmp(node->value, "+") == 0) {
+            fprintf(compiler->output, "    add rax, rbx\n");
+        }
+        for (int i = 0; i < compiler->var_count; i++) {
+            if (strcmp(var_map[i], node->left->value) == 0) {
+                fprintf(compiler->output, "    mov [rsp-%d], rax\n", (i + 1) * 8);
+                break;
+            }
+        }
+    } else if (node->type == NODE_WRITELINE) {
+        if (node->left->type == NODE_BINARY_OP) {
+            fprintf(compiler->output, "    mov rsi, %s\n", node->left->value);
+        } else {
+            for (int i = 0; i < compiler->var_count; i++) {
+                if (strcmp(var_map[i], node->left->value) == 0) {
+                    fprintf(compiler->output, "    mov rsi, [rsp-%d]\n", (i + 1) * 8);
+                    break;
+                }
+            }
+        }
+        fprintf(compiler->output, "    mov rax, 1\n"); // syscall: write
+        fprintf(compiler->output, "    mov rdi, 1\n"); // stdout
+        fprintf(compiler->output, "    mov rdx, 1\n"); // length
+        fprintf(compiler->output, "    syscall\n");
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <input.dr>\n", argv[0]);
+        return 1;
+    }
+
+    // Read source file
+    FILE* file = fopen(argv[1], "r");
+    if (!file) {
+        fprintf(stderr, "Cannot open file %s\n", argv[1]);
+        return 1;
+    }
+    fseek(file, 0, SEEK_END);
+    long size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    char* source = malloc(size + 1);
+    fread(source, 1, size, file);
+    source[size] = '\0';
+    fclose(file);
+
+    // Initialize lexer and compiler
+    Lexer lexer = {source, 0, 1};
+    Compiler compiler = {fopen("output.asm", "w"), 0};
+
+    // Write assembly preamble
+    fprintf(compiler.output, "section .text\n");
+    fprintf(compiler.output, "global _start\n");
+    fprintf(compiler.output, "_start:\n");
+    fprintf(compiler.output, "    sub rsp, 1024\n"); // Reserve stack space
+
+    // Parse and generate code
+    Token token = next_token(&lexer);
+    while (token.type != TOKEN_EOF) {
+        Node* node = parse_statement(&lexer, &token);
+        generate_assembly(&compiler, node);
+        free(node->value);
+        if (node->left) free(node->left->value), free(node->left);
+        if (node->right) free(node->right->value), free(node->right);
+        free(node);
+        token = next_token(&lexer);
+    }
+
+    // Write assembly epilogue
+    fprintf(compiler.output, "    mov rax, 60\n"); // syscall: exit
+    fprintf(compiler.output, "    mov rdi, 0\n");
+    fprintf(compiler.output, "    syscall\n");
+
+    fclose(compiler.output);
+    free(source);
+    free(token.value);
+
+    // Assemble and link
+    system("nasm -f elf64 output.asm -o output.o");
+    system("ld output.o -o dream");
+    return 0;
+}
