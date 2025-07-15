32081d6699513b977182c4f90581faafb37ce157
Damon Fedorick <95268795+Ddemon26@users.noreply.github.com>
2025-07-15 01:34:41 -0700

    idea: add color settings and code style

---
+++ idea/src/main/java/com/dream/DreamCodeStyleSettings.java
@@ -0,0 +1,10 @@
+package com.dream;
+
+import com.intellij.psi.codeStyle.CodeStyleSettings;
+import com.intellij.psi.codeStyle.CustomCodeStyleSettings;
+
+public class DreamCodeStyleSettings extends CustomCodeStyleSettings {
+    protected DreamCodeStyleSettings(CodeStyleSettings settings) {
+        super("DreamCodeStyleSettings", settings);
+    }
+}
