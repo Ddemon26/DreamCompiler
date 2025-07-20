d72363d74e569f61435195438351d0dc2bbc8da3
Damon Fedorick <95268795+Ddemon26@users.noreply.github.com>
2025-07-20 00:52:50 -0700

    Add platform-specific Vulkan loader

--- src/runtime/vulkan_stub.h
+++ src/runtime/vulkan_stub.h
@@ -1,6 +1,20 @@
 #ifndef DR_VULKAN_STUB_H
 #define DR_VULKAN_STUB_H
 
+#include <stdint.h>
+#include <vulkan/vulkan_core.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int dr_vulkan_available(void);
+uint32_t dr_vulkan_version(void);
+PFN_vkVoidFunction dr_load_instance_proc(VkInstance instance, const char *name);
+PFN_vkVoidFunction dr_load_device_proc(VkDevice device, const char *name);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // DR_VULKAN_STUB_H
